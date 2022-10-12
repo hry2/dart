2d4620251bf36d27462d614c3b8cd6b89a060567
hry2 <79210445+hry2@users.noreply.github.com>
2022-10-12 19:27:22 +0200

    Update exo11.dart

--- exo11.dart
+++ exo11.dart
@@ -1,7 +1,4 @@
-/* Je boss toujours la dessus !
-
-
-Un programme principal saisit une chaîne d'ADN valide et une séquence d'ADN valide (valide signifie qu'elles ne sont pas vides
+/* Un programme principal saisit une chaîne d'ADN valide et une séquence d'ADN valide (valide signifie qu'elles ne sont pas vides
  et sont formées exclusivement d'une combinaison arbitraire de "a", "t", "g" ou "c").
 
 Écrire une fonction valide qui renvoie vrai si la saisie est valide, faux sinon.
@@ -30,13 +27,26 @@ bool valide(String sequenceEntre) {
     return false;
 }
 
-void saisie(String chaine, String sequence) {
+double saisie(String chaine, String sequence) {
   //double proportionDeLaSequenceDansLaChaine ;
-  print( sequence.allMatches(chaine));
-  
-  //return proportionDeLaSequenceDansLaChaine;
+  String currentSequence = '';
+  // ignore: unused_local_variable
+  bool finDeLaChaine = false;
+  int biginningOfCurrentSequence = 0;
+  int match = 0;
+  while (finDeLaChaine = false) {
+    for (int i = biginningOfCurrentSequence; i <= sequence.length; i++) {
+      currentSequence += chaine[i];
+    }
+    if (currentSequence == sequence) {match++;}
+    biginningOfCurrentSequence  ++;
+    if (biginningOfCurrentSequence == chaine.length) {finDeLaChaine = true;}
+    //return proportionDeLaSequenceDansLaChaine;
+  }
+  double proportionDeLaSequenceDansLaChaine = (match*sequence.length*100/chaine.length);
+  return proportionDeLaSequenceDansLaChaine ;
 }
 
 void main() {
-  saisie('abcdabeg', 'ab');
+//récpérer la chaine et la séquence à comparer
 }
